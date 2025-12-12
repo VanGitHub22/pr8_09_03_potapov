73d32a3e8d5a62c21666304eb40405cdddc9641a
DikiYLexa <dubovan36@gmail.com>
2025-12-12 10:38:53 +0500

    Проверка пароля на стотроне сервера. Потапов

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -4,6 +4,11 @@
 	
 	$login = $_POST['login'];
 	$password = $_POST['password'];
+
+	$check_password = preg_match('/^(?=.*[0-9])(?=.*[!@#$%^&?*\-_=])(?=.*[a-z])(?=.*[A-Z])[0-9a-z-A-Z!@#$%^&?*\-_=]{8,}$/', $password);
+	if($check_password == false){
+		exit;
+	}
 	
 	// ищем пользователя
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
